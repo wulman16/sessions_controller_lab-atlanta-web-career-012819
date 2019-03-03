82a664ccaf7f2a634910fd4f2ecf58979e83016e
wulman16 <will.ulman@gmail.com>
2019-03-03 17:29:47 -0500

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,18 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    if params[:name].nil? || params[:name].empty?
+      redirect_to sessions_new_path
+    else
+      session[:name] = params[:name]
+      redirect_to root_path
+    end
+  end
+
+  def destroy
+    session.delete :name
+  end
+
+end
